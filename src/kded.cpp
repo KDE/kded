5d0a3c2a167590f43a7603dedcb49bfe2a5d667a
David Faure <faure@kde.org>
2015-12-02 08:24:18 +0100

    Fix compiler warning (and trigger CI again)

--- src/kded.cpp
+++ src/kded.cpp
@@ -47,7 +47,7 @@
 #include <KPluginInfo>
 #include <KPluginMetaData>
 
-Q_DECLARE_LOGGING_CATEGORY(KDED);
+Q_DECLARE_LOGGING_CATEGORY(KDED)
 
 #if QT_VERSION >= QT_VERSION_CHECK(5, 4, 0)
 // logging category for this framework, default: log stuff >= warning
